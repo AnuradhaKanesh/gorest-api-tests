e24bcbdb5ca33b0de10a9762528cf955e31f5c0b
AnuradhaKanesh <anuradhak@testvagrant.com>
2022-02-16 00:31:22 +0530

    added testcase shouldCreateUser

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,5 +15,20 @@ public class SampleTest {
                 .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void shouldCreateUser()
+    {
+        given()
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .header("Authorization","Bearer 72c04051e13a737af16869169867c00064b1515c8ed9c1578ebfca8946c116fe")
+                    .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.krishaniyer@gmail.com\", \"status\":\"active\"}")
+                .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                .then()
+                    .statusCode(201)
+                    .log().body();
     }
 }
