dd50647f1656d2b8194222e8469b31be63730ebd
AnuradhaKanesh <anuradhak@testvagrant.com>
2022-02-16 01:12:27 +0530

    added assert name in shouldCreateUser

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -26,13 +26,14 @@ public class SampleTest {
                     .accept(ContentType.JSON)
                     .contentType(ContentType.JSON)
                     .header("Authorization","Bearer 72c04051e13a737af16869169867c00064b1515c8ed9c1578ebfca8946c116fe")
-                    .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.krishaniyer3@gmail.com\", \"status\":\"active\"}")
+                    .body("{\"name\":\"Tenali Ramakrishna\", \"gender\":\"male\", \"email\":\"tenali.krishaniyer5@gmail.com\", \"status\":\"active\"}")
                 .when()
                     .post("https://gorest.co.in/public/v2/users")
                 .then()
                     .log().body()
                     .statusCode(201)
                 .body("id",Matchers.notNullValue())
-                .body("email",Matchers.equalTo("tenali.krishaniyer3@gmail.com"));
+                .body("email",Matchers.equalTo("tenali.krishaniyer5@gmail.com"))
+                .body("name",Matchers.equalTo("Tenali Ramakrishna"));
     }
 }
