92519bb1cc0f5ea3b41923e46ddba97c218bf65e
AnuradhaKanesh <anuradhak@testvagrant.com>
2022-02-15 23:50:57 +0530

    added first test getAllUsers

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,18 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class SampleTest {
 
     @Test
-    public void verifyTest()
+    public void shouldGetAllUsers()
     {
-        Assert.assertEquals(1,1);
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v2/users")
+                .then()
+                .statusCode(200)
+                .log().body();
     }
 }
